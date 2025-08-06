55e519a477283aaed3e9f920727b242969f5a0c9
Hyungseok Ko <kohs100@camelab.org>
2025-08-07 06:17:07 +0900

    asdf

--- 3_upload_supabase.py
+++ 3_upload_supabase.py
@@ -15,7 +15,21 @@ DAY_EVENT={
     "土": 1,
     "日": 2
 }
-BATCH_SIZE=32
+CIRCLE_TYPE=1
+BATCH_SIZE=64
+
+def batch_insert(supabase: Client, rows: List[Dict[str, Any]], table: str):
+    num_batch = len(rows) // BATCH_SIZE + 1
+
+    for batch_idx in trange(num_batch):
+        idx_begin = batch_idx * BATCH_SIZE
+        idx_end = idx_begin + BATCH_SIZE
+        if len(rows) < idx_end:
+            idx_end = len(rows)
+        try:
+            supabase.table(table).upsert(rows[idx_begin:idx_end]).execute() # type: ignore
+        except Exception as e:
+            print(e)
 
 def main():
     url: str = os.getenv("SUPABASE_URL", default="")
@@ -27,30 +41,51 @@ def main():
     with open("dict/id_dict.pkl", "rb") as f:
         id_dict: Dict[int, Circle] = pickle.load(f)
 
-    rows: List[Dict[str, Any]] = []
+    circles: Dict[int, str] = {}
+
+    rows_booth: List[Dict[str, Any]] = []
     for _id, circle in id_dict.items():
+        cid = circle.CircleId
+        if circle.CircleId > 0:
+            assert cid not in circles
+            circles[cid] = circle.Name
+
+        space = normalize('NFKC', circle.Space)
+        assert space == circle.Space
+
+        space_num = space[:2]
+        assert int(space_num) >= 0
+        space_subsec = space[2:]
+        assert space_subsec == 'a' or space_subsec == 'b'
+
+        if circle.Is2SP:
+            space_subsec = 'ab'
+        locstr = f'{circle.Block}{space_num}{space_subsec}'
+
         row: Dict[str, Any] = {
             "event_id": DAY_EVENT[circle.Day],
             "location_top": circle.Hall,
-            "location": normalize('NFKC', circle.Block + circle.Space),
+            "location": locstr,
             "jname": circle.Name,
             "data": circle.model_dump(),
+            "circle_id": circle.CircleId,
+            "circle_type": CIRCLE_TYPE
         }
-        rows.append(row)
+        rows_booth.append(row)
 
-    print(len(rows))
+    rows_circle:List[ Dict[str, Any]] = []
+    for cid, name in circles.items():
+        rows_circle.append({
+            "circleid": cid,
+            "circle_type": CIRCLE_TYPE,
+            "name": name
+        })
 
-    num_batch = len(rows) // BATCH_SIZE + 1
+    print("# of circles:", len(rows_booth))
+    print("# of booths:", len(rows_circle))
 
-    for batch_idx in trange(num_batch):
-        idx_begin = batch_idx * BATCH_SIZE
-        idx_end = idx_begin + BATCH_SIZE
-        if len(rows) < idx_end:
-            idx_end = len(rows)
-        try:
-            supabase.table("booth").insert(rows[idx_begin:idx_end]).execute() # type: ignore
-        except Exception as e:
-            print(e)
+    # batch_insert(supabase, rows_circle, "circle")
+    batch_insert(supabase, rows_booth, "booth")
 
 if __name__ == "__main__":
     main()
